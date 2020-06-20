c7499f140444ae89363347e0f7eed50ee274774d
Ionut Rogojan <34418533+ionutrogojan@users.noreply.github.com>
2020-06-20 13:20:25 +0100

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,46 +1,90 @@
+//Set-Location -Path C:\Users\irogo\Desktop\pizzaJoe
+
 const Discord = require('discord.js');
-const config = require('./config.json');
-const disco = new Discord.Client();
-const prefix = config.prefix;
-const allowedUsers = config.allowedUsers;
-const roles = config.roleToDisco;
+const client = new Discord.Client();
+const prefixes = ["p!","joe"];
 
-disco.on("ready", () => {
-    disco.user.setPresence({ game: { name: `Disco Roles! Created by i am toast#1213` }, type: 0 });
-    console.log("Disco role bot online! Created by i am toast.");
-});
-
-disco.on("message", message => {
-
-  function discoRole() {
-    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    roles.forEach((role) => {
-      let theRole = message.guild.roles.find("name", role);
-      theRole.edit({color: random}).catch(e => {
-        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
-      });
-    });
-  }
-
-  if(message.content.startsWith(prefix + "startdisco")) {
-    if(allowedUsers.includes(message.author.id)) {
-    setInterval(() => { discoRole(); }, config.ms);
-    message.channel.send("```css\nDiscoing...```");
-    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
-  } else {
-    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-} else
-
-if(message.content.startsWith(prefix + "stopdisco")) {
-  if(allowedUsers.includes(message.author.id)) {
-  message.channel.send("I've stopped discoing.");
-  setTimeout(() => { console.log(process.exit(0)); }, 300);
-} else {
-  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
+function catchError (error, message) {
+	message.channel.send(`Be more specific, I'm to cool to solve that :sunglasses:!`);
 }
 
+client.once('ready', () => {
+	console.log('Ready!');
+		client.user.setStatus('idle');
+		client.user.setActivity('p!help | version 1.0.2', {
+				type: 'PLAYING'
+		});
 });
 
-disco.login(config.token);
+client.on('message', message => {
+  if(message.member.hasPermission(['SEND_MESSAGES', 'VIEW_CHANNEL'])) {
+		if(message.author.bot) return;
+
+		let thereIsPrefix = false;
+		// For each element of prefixes
+		for (i=0; i<prefixes.length; i++){
+    // if message starts with prefix
+    // Note that here is not 《!》 unlinke before
+    if (message.content.toLowerCase().startsWith(prefixes[i])) {
+    thereIsPrefix = true;
+    // remove prefix
+    message.content = message.content.slice(0 + prefixes[i].length );
+
+    // we did our job, so we exit loop
+    break;
+    }
+}
+		function pizzaRole() {
+				let botRole = message.guild.role.find(role => role.name === "Pizza Joe");
+				botRole.edit({color: "#a9c52f"});
+		}
+
+			try {
+
+			  	//console.log(message.content);
+			    if(message.content.startsWith(`help`)) {
+						const helpEmbed = new Discord.MessageEmbed()
+							.setColor('#fe9801')
+							.setTitle('Pizza Joe commands list')
+							.setURL('https://www.youtube.com/watch?v=lpvT-Fciu-4')
+							.setDescription("**HERE ARE THE AVAILABLE COMMANDS**\n Pizza Joe will answer to both `p!` and `Joe`\n \n `p!help` - display commands list\n `p!pizza?` - Pizza Joe will answer\n `p!morning @user` - Pizza Joe will say Morning\n`p!night @user` - Pizza Joe will say Good Night\n `p!pizza @user` - Bake someone a pizza (requires @user)\n `p!joe` - Ping Pizza Joe\n `p!love` - Pizza Joe will love you back\n `Joe I want pizza` - Pizza joe will answer\n `Joe you're a dummy` - Pizza Joe will answer back\n \n **USEFUL LINKS**\n [INVITE ME](https://discord.com/oauth2/authorize?client_id=723293726515921027&permissions=412736&scope=bot) | [SUPPORT](https://discord.com/invite/tEaZ2c7)")
+							.setFooter(`Pizza Joe is the best!`)
+			      message.channel.send(helpEmbed);
+			    }
+					else if(message.content.startsWith(`pizza?`)) {
+			      message.channel.send("Always Pizza! https://gph.is/2ferErB")
+			    }
+					else if(message.content.startsWith(`morning`)){
+			      const taggedUser = message.mentions.users.first();
+			      	message.channel.send(`Good Morning ${taggedUser.username}! You should have some :pizza: ***pizza***! https://giphy.com/gifs/3orieNTh5JZFPYZXR6 `)
+			    }
+					else if(message.content.startsWith(`night`)){
+			      const taggedUser = message.mentions.users.first();
+			      	message.channel.send(`Good Night ${taggedUser.username}! May your dreams be full of :pizza: ***pizza***! https://gph.is/2ILP1Gd `)
+			    }
+					else if(message.content.startsWith(`pizza`)){
+			      const taggedUser = message.mentions.users.first();
+			      	message.channel.send(`You baked ${taggedUser.username} a :pizza: pizza. Enjoy!`)
+			    }
+					else if(message.content.startsWith(`joe`)) {
+						message.channel.send(`Stop pinging Pizza Joe you :poop:!`)
+					}
+					else if(message.content.startsWith(`love`)) {
+						message.channel.send(`Here you go sugar :heart:! https://gph.is/g/ZdxNGvP`)
+					}
+					else if(message.content.startsWith(` you're a dummy`)) {
+						message.channel.send(`No, U! https://i.imgur.com/mUBjN1a.jpg`)
+					}
+					else if(message.content.startsWith(` I want pizza`)) {
+						message.channel.send(`Me too! https://gph.is/19iU3Rq`)
+					}
+
+
+			} catch (error) {
+						catchError(error, message);
+											}
+	}
+
+});
+
+client.login(process.env.TOKEN);
